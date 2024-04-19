145698fe38abb559fb6774d262c16ab0a0fe7f7d
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-04-20 00:01:30 +0200

    Add daisyui package

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+import daisyui from "daisyui";
+
+export default {
+    plugins: [forms, daisyui],
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    daisyui: {
+        themes: ["night"],
+        darkTheme: "night",
+    },
+} satisfies Config;
