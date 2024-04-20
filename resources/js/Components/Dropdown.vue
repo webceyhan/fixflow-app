76b188217d955af9a462f80b0ea32909ce5193ec
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-04-20 02:18:25 +0200

    Migrate dropdown components

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -1,75 +1,39 @@
 <script setup lang="ts">
-import { computed, onMounted, onUnmounted, ref } from 'vue';
-
-const props = withDefaults(
-    defineProps<{
-        align?: 'left' | 'right';
-        width?: '48';
-        contentClasses?: string;
-    }>(),
-    {
-        align: 'right',
-        width: '48',
-        contentClasses: 'py-1 bg-white',
-    }
-);
-
-const closeOnEscape = (e: KeyboardEvent) => {
-    if (open.value && e.key === 'Escape') {
-        open.value = false;
-    }
-};
-
-onMounted(() => document.addEventListener('keydown', closeOnEscape));
-onUnmounted(() => document.removeEventListener('keydown', closeOnEscape));
-
-const widthClass = computed(() => {
-    return {
-        48: 'w-48',
-    }[props.width.toString()];
-});
-
-const alignmentClasses = computed(() => {
-    if (props.align === 'left') {
-        return 'ltr:origin-top-left rtl:origin-top-right start-0';
-    } else if (props.align === 'right') {
-        return 'ltr:origin-top-right rtl:origin-top-left end-0';
-    } else {
-        return 'origin-top';
-    }
-});
-
-const open = ref(false);
+defineProps<{
+  label?: string;
+  alignEnd?: boolean;
+  contentClasses?: string;
+}>();
 </script>
 
 <template>
-    <div class="relative">
-        <div @click="open = !open">
-            <slot name="trigger" />
-        </div>
-
-        <!-- Full Screen Dropdown Overlay -->
-        <div v-show="open" class="fixed inset-0 z-40" @click="open = false"></div>
-
-        <Transition
-            enter-active-class="transition ease-out duration-200"
-            enter-from-class="opacity-0 scale-95"
-            enter-to-class="opacity-100 scale-100"
-            leave-active-class="transition ease-in duration-75"
-            leave-from-class="opacity-100 scale-100"
-            leave-to-class="opacity-0 scale-95"
+  <div :class="['dropdown', { 'dropdown-end': alignEnd }]">
+    <!-- trigger -->
+    <slot name="trigger">
+      <div tabindex="0" role="button" class="btn m-1">
+        {{ label }}
+        <svg
+          class="ms-2 -me-0.5 h-4 w-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
-            <div
-                v-show="open"
-                class="absolute z-50 mt-2 rounded-md shadow-lg"
-                :class="[widthClass, alignmentClasses]"
-                style="display: none"
-                @click="open = false"
-            >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
-                    <slot name="content" />
-                </div>
-            </div>
-        </Transition>
-    </div>
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+    </slot>
+
+    <!-- content -->
+    <ul
+      tabindex="0"
+      class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+      :class="contentClasses"
+    >
+      <slot />
+    </ul>
+  </div>
 </template>
