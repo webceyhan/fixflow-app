03c452b59bac9cdf9e2dc19d36696881bf6a44db
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-04-20 15:55:13 +0200

    Convert responsive navbar to sidebar

---
+++ resources/js/Components/Drawer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps({
+  id: String,
+});
+</script>
+
+<template>
+  <div class="drawer lg:drawer-open">
+    <!-- internal toggle -->
+    <input :id="id" type="checkbox" class="drawer-toggle" />
+
+    <div class="drawer-content">
+      <!-- Page content here -->
+      <slot />
+    </div>
+
+    <aside class="drawer-side">
+      <!-- backdrop toggle -->
+      <label :for="id" aria-label="close sidebar" class="drawer-overlay" />
+
+      <div class="p-4 w-80 min-h-full bg-base-200 text-base-content flex flex-col">
+        <slot name="aside" />
+      </div>
+    </aside>
+  </div>
+</template>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { Link } from "@inertiajs/vue3";
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
@@ -7,8 +6,7 @@ import Dropdown from "@/Components/Dropdown.vue";
 import MenuLink from "@/Components/MenuLink.vue";
 import Menu from "@/Components/Menu.vue";
 import Navbar from "@/Components/Navbar.vue";
-
-const showingNavigationDropdown = ref(false);
+import Drawer from "@/Components/Drawer.vue";
 </script>
 
 <template>
@@ -16,19 +14,10 @@ const showingNavigationDropdown = ref(false);
     <div class="min-h-screen">
       <Navbar class="gap-2 sm:px-4">
         <!-- Logo -->
-        <div class="max-sm:flex-1">
+        <div class="flex-1">
           <Link :href="route('dashboard')">
             <ApplicationLogo class="block h-9 w-auto fill-current px-2" />
           </Link>
-        </div>
-
-        <!-- Navigation Links -->
-        <div class="max-sm:hidden flex-1">
-          <Menu horizontal>
-            <MenuLink :href="route('dashboard')" :active="route().current('dashboard')">
-              Dashboard
-            </MenuLink>
-          </Menu>
         </div>
 
         <!-- Settings Dropdown -->
@@ -39,81 +28,72 @@ const showingNavigationDropdown = ref(false);
           </Dropdown>
         </div>
 
-        <!-- Hamburger -->
-        <div class="flex sm:hidden">
-          <button
-            @click="showingNavigationDropdown = !showingNavigationDropdown"
-            class="btn"
-          >
-            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-              <path
-                :class="{
-                  hidden: showingNavigationDropdown,
-                  'inline-flex': !showingNavigationDropdown,
-                }"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-              <path
-                :class="{
-                  hidden: !showingNavigationDropdown,
-                  'inline-flex': showingNavigationDropdown,
-                }"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
+        <!-- Sidebar Toggle -->
+        <label for="sidebar" class="btn drawer-button lg:hidden">
+          <!-- hamburger icon -->
+          <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+          <!-- close icon -->
+          <!-- <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg> -->
+        </label>
       </Navbar>
 
-      <!-- Responsive Navigation Menu -->
-      <Navbar v-if="showingNavigationDropdown" class="sm:hidden">
-        <Menu class="flex-1 w-full">
-          <MenuLink
-            :href="route('dashboard')"
-            :active="route().current('dashboard')"
-            class="w-full"
-          >
-            Dashboard
-          </MenuLink>
+      <Drawer id="sidebar">
+        <template #aside>
+          <!-- Navigation Links -->
+          <Menu class="mb-auto">
+            <MenuLink :href="route('dashboard')" :active="route().current('dashboard')">
+              Dashboard
+            </MenuLink>
+          </Menu>
 
           <!-- Responsive Settings Options -->
-          <div class="w-full pt-4 px-4">
-            <div class="font-medium text-base text-gray-800">
-              {{ $page.props.auth.user.name }}
-            </div>
-            <div class="font-medium text-sm text-gray-500">
-              {{ $page.props.auth.user.email }}
-            </div>
+          <div class="sm:hidden divider" />
+
+          <Menu class="sm:hidden">
+            <li>
+              <h2 class="menu-title">
+                <p class="text-base text-base-content">
+                  {{ $page.props.auth.user.name }}
+                </p>
+                {{ $page.props.auth.user.email }}
+              </h2>
+
+              <ul>
+                <MenuLink :href="route('profile.edit')"> Profile </MenuLink>
+                <MenuLink :href="route('logout')" method="post" as="button">
+                  Log Out
+                </MenuLink>
+              </ul>
+            </li>
+          </Menu>
+        </template>
+
+        <!-- Page Heading -->
+        <header class="bg-neutral text-neutral-content shadow" v-if="$slots.header">
+          <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <slot name="header" />
           </div>
+        </header>
 
-          <li class="w-full">
-            <ul class="w-full">
-              <MenuLink :href="route('profile.edit')"> Profile </MenuLink>
-              <MenuLink :href="route('logout')" method="post" as="button">
-                Log Out
-              </MenuLink>
-            </ul>
-          </li>
-        </Menu>
-      </Navbar>
-
-      <!-- Page Heading -->
-      <header class="bg-neutral text-neutral-content shadow" v-if="$slots.header">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <slot name="header" />
-        </div>
-      </header>
-
-      <!-- Page Content -->
-      <main>
-        <slot />
-      </main>
+        <!-- Page Content -->
+        <main>
+          <slot />
+        </main>
+      </Drawer>
     </div>
   </div>
 </template>
