078d5b7c65f235eb11baea74e03cdf68b7ed3d40
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-04-21 23:25:23 +0200

    Refactor text-input component

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 
+/**
+ * DiasyUI classes
+ */
+interface Props {}
+
 const model = defineModel<string>({ required: true });
 
 const input = ref<HTMLInputElement | null>(null);
+
+defineExpose({ focus: () => input.value?.focus() });
 
 onMounted(() => {
   if (input.value?.hasAttribute("autofocus")) {
     input.value?.focus();
   }
 });
-
-defineExpose({ focus: () => input.value?.focus() });
 </script>
 
 <template>
-  <input class="input input-bordered w-full" v-model="model" ref="input" />
+  <input ref="input" type="text" class="input input-bordered w-full" v-model="model" />
 </template>
