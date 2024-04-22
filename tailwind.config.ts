4667c3206c92a5c67006a37ed2f406bab205a7d4
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-04-22 13:02:54 +0200

    Add custom daisyui theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
 import daisyui from "daisyui";
+import daisyuiThemes from "daisyui/src/theming/themes";
 
 export default {
     plugins: [forms, daisyui],
@@ -21,7 +22,15 @@ export default {
     },
 
     daisyui: {
-        themes: ["night"],
-        darkTheme: "night",
+        themes: [
+            {
+                fixflow: {
+                    ...daisyuiThemes.night,
+                    primary: daisyuiThemes.night.secondary,
+                    secondary: daisyuiThemes.night.neutral,
+                },
+            },
+        ],
+        darkTheme: "fixflow",
     },
 } satisfies Config;
