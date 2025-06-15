cba0c6f27b7edb458da3e28cc5c814328ccb983d
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-15 22:21:49 +0200

    Update brand name to Fixflow #25

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -31,7 +31,7 @@ const footerNavItems: NavItem[] = [
 </script>
 
 <template>
-    <Sidebar collapsible="icon" variant="inset">
+    <Sidebar collapsible="icon" variant="sidebar">
         <SidebarHeader>
             <SidebarMenu>
                 <SidebarMenuItem>
