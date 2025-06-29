162a4065e54ac0ab3077a06029104d305ad458de
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-30 00:48:14 +0200

    Add strict models configuration #4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Enable strict mode for Eloquent models to catch N+1 queries and other issues
+        Model::shouldBeStrict();
     }
 }
