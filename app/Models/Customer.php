38528192e95a810c290dd2be9bbeb61c756d22ce
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-07-01 01:37:38 +0200

    fix customers

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
+    /** @use HasFactory<\Database\Factories\CustomerFactory> */
     use HasFactory;
 
     /**
