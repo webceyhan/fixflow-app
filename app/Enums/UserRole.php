56fdd1ce6aa5b6b2b43ac6b01b6b77de28b59631
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-30 12:26:47 +0200

    Create User Entity - Migration, Model, and Factory #7

---
+++ app/Enums/UserRole.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Enums;
+
+use App\Enums\Concerns\HasValues;
+
+enum UserRole: string
+{
+    use HasValues;
+
+    /**
+     * Represents the administrator with full access to the application.
+     */
+    case Admin = 'admin';
+
+    /**
+     * Represents a manager responsible for overseeing operations and managing employees.
+     */
+    case Manager = 'manager';
+
+    /**
+     * Represents a technician responsible for handling technical tasks and repairs.
+     *
+     * @default
+     */
+    case Technician = 'technician';
+
+    // METHODS /////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Determine if the user is an administrator.
+     */
+    public function isAdmin(): bool
+    {
+        return $this === self::Admin;
+    }
+}
