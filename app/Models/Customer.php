c10effb9788327b1e642cc5516e0090f88ee5c06
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-30 17:27:13 +0200

    Create Customer Entity - Migration, Model, and Factory #8

---
+++ app/Models/Customer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    /** @use HasFactory<\Database\Factories\CustomerFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'company',
+        'vat_number',
+        'email',
+        'phone',
+        'address',
+        'note',
+    ];
+}
