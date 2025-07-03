fd7d72673c642ce8c33ec6bce3028fe927b82f89
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-07-03 22:42:04 +0200

    Create Invoice Entity - Migration, Model, and Factory #14

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             TicketSeeder::class,
             TaskSeeder::class,
             OrderSeeder::class,
+            InvoiceSeeder::class,
         ]);
     }
 }
